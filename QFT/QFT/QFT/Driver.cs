4bf3faac05349d2350c89fba04a7a52786bb3cc7
Anita Ramanan <anraman@microsoft.com>
2018-07-04 22:24:20 +0100

    Added QFT operations

--- QFT/QFT/QFT/Driver.cs
+++ QFT/QFT/QFT/Driver.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
+using System.Diagnostics;
 
-namespace QFT
+namespace Quantum.QFT
 {
     class Driver
     {
         static void Main(string[] args)
         {
-
+            //using (var sim = new QuantumSimulator())
+            //{
+            //    var count = LoopTest.Run(sim).Result;
+            //    Debug.WriteLine(count);
+            //}
         }
     }
 }
